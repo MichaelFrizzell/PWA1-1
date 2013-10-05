2866696f24dd342c9171a17c61f3d1c3ad944729
James Michiemo <omeichim@gmail.com>
2013-10-05 13:49:12 -0400

    correct mispelling of validate variable

--- MichiemoJames/Wk2/debug/js/search.js
+++ MichiemoJames/Wk2/debug/js/search.js
@@ -4,11 +4,10 @@
 	// Variable initialization (DO NOT FIX ANY OF THE BELOW VAR's)
 	var resultsDIV = document.getElementById("results"),
 		searchInput = document.forms[0].search,
-		currentSearch = ''
-	;
+		currentSearch = '';
 	
 	// Validates search query
-	var validqte == function(query){
+	var validate == function(query){
 		
 		// Trim whitespace from start and end of search query
 		while(query.charAt(0) = " "){
@@ -110,7 +109,7 @@
 	// THE LINE DIRECTLY BELOW IS CORRECT
 	document.forms[0].onsubmit = function(){
 		var query = searchInput.value;
-		validqte(query);
+		validate(query);
 
         // return false is needed for most events - this will be reviewed in upcoming course material
         // THE LINE DIRECTLY BELOW IS CORRECT
