dc843cd08730c9baf669517505e109483eccc820
James Michiemo <omeichim@gmail.com>
2013-10-09 18:13:10 -0400

    create variable to store a random integer

--- MichiemoJames/Wk2/guess/js/main.js
+++ MichiemoJames/Wk2/guess/js/main.js
@@ -1,14 +1,17 @@
 /**
  * GUESSING GAME:
  *
- * Created By: 
- * Date: 
+ * Created By: James Michiemo
+ * Date: October 9, 2013 
  * 
  * GUESSING GAME
  */
 
 //Game variables
-(function ()
+(function (){
+  var random = ~~(Math.random() * 11); //Create a variable to store a random integer within the range 1-10.
+  console.log("Random number: ", random);
+ 
 
 
 })();
