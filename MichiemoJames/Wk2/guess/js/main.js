3965aef0a83a6db237534a9290a7e7bd5e1f9399
James Michiemo <omeichim@gmail.com>
2013-10-09 19:29:31 -0400

    create an event listener on the guess button to listen for a click event

--- MichiemoJames/Wk2/guess/js/main.js
+++ MichiemoJames/Wk2/guess/js/main.js
@@ -11,11 +11,17 @@
 (function (){
  var random = ~~(Math.random() * 11), //Create a variable to store a random integer within the range 1-10.
      query = document.getElementById("input"), 
-     result = document.getElementById("output"); //Create variables that reference the DOM(html) elements. 
+     result = document.getElementById("output"), //Create variables that reference the DOM(html) elements.
+     guessButton = document.querySelector('button');
  console.log("Random number: ", random);
  console.log("Input element: ", query);
  console.log("Output element: ", output);
 
+ function button_click(event) {
+   console.log("Button clicked");
+ }
 
+ guessButton.addEventListener('click', button_click, false); // Create an .addEventListner event listener on the guess button to listen for a click event. 
 
-})();
+})(); 
+
