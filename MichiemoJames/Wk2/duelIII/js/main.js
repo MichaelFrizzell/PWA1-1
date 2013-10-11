114f9add2b40d4dc978f0bfb197cea3af27c3ab5
James Michiemo <omeichim@gmail.com>
2013-10-10 23:33:53 -0400

    replace alerts wi alerts with console.log and retarget properties within objects

--- MichiemoJames/Wk2/duelIII/js/main.js
+++ MichiemoJames/Wk2/duelIII/js/main.js
@@ -9,38 +9,49 @@
   var fighters = [
     playerOne = {
       name: "Ibuki",
-      damage:300,
-      health:50
+      health:300,
+      damage:50
     },
     playerTwo = {
       name: "Blanka",
-      damage:300,
-      health:50
+      health:300,
+      damage:50
     }
   ],
     round = 0;
 
+
+  var playerOneHP = document.querySelector('#kabal p');
+
+  var playerTwoHP = document.querySelector('#kratos p');
+
+  playerOneHP.innerHTML = fighters[0].health;
+  playerTwoHP.innerHTML = fighters[1].health;
+
   function fight(){
+    console.log(fighters[0].name + ":" + fighters[0].health + "  VS  " + fighters[1].name + ":" + fighters[1].health);
     
     for (var i = 0; i < 10; i++)
     { 
       /* instantiate damage */
-      var minDmg1 = playerOne[2] * .5,
-          minDmg2 = playerTwo[2] * .5,
-          hit1 = Math.floor(Math.random()*(playerOne[2]-minDmg1)+minDmg1),
-          hit2 = Math.floor(Math.random()*(playerTwo[2]-minDmg2)+minDmg2);
+      var minDmg1 = fighters[0] * .5,
+          minDmg2 = fighters[0] * .5,
+          hit1 = ~~(Math.random()*(fighters[0].damage-minDmg1)+minDmg1),
+          hit2 = ~~(Math.random()*(fighters[1].damage-minDmg2)+minDmg2);
 
       /* execute damage */
-      playerOne[1]-=hit1;
-      playerTwo[1]-=hit2;
+      fighters[0].health-=hit1;
+      fighters[1].health-=hit2;
 
       /* check for KO */
       var result = winnerCheck();
       if (result === "noKO")
       {
         round++;
+        console.log(fighters[0].name + ":" + fighters[0].health + "     =====Round " + round + "! Fight!=====     " + fighters[1].name + ":" + fighters[1].health);
       } else
       {
+        console.log(result);
         break;
       };
     };
@@ -48,15 +59,15 @@
 
   function winnerCheck(){
     var result = "noKO";
-    if (playerOne[1] < 1 && playerTwo[1] < 1)
+    if (fighters[0].health < 1 && fighters[1].health < 1)
     {
       result = "Draw!";
-    } else if (playerOne[1] < 1)
+    } else if (fighters[1].health < 1)
     {
-      result = playerTwo[0] + " Wins!";
-    } else if (playerTwo[1] < 1)
+      result = fighters[0].name + " Wins!";
+    } else if (fighters[0].health < 1)
     {
-      result = playerOne[0] + " Wins!";
+      result = fighters[1].name + " Wins!";
     };
     return result;
   };
