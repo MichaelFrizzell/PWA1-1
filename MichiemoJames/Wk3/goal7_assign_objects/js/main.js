c7682a2ac69c84fe0f155edcd2301f21efd2cc3a
James Michiemo <james.michiemo@gmail.com>
2013-10-19 01:13:57 -0400

    place name, date, and assignment at the top of both JavaScript files

--- MichiemoJames/Wk3/goal7_assign_objects/js/main.js
+++ MichiemoJames/Wk3/goal7_assign_objects/js/main.js
@@ -2,5 +2,9 @@
  * Created by the JavaScript Development Team
  * Class: PWA
  * Goal: Goal7
+ * Name: James Michiemo
+ * Date: 19OCT2013
  */
+
+
 
--- MichiemoJames/Wk3/goal7_assign_objects/js/person.js
+++ MichiemoJames/Wk3/goal7_assign_objects/js/person.js
@@ -2,4 +2,6 @@
  * Created by the JavaScript Development Team
  * Class: PWA
  * Goal: Goal7
+ * Name: James Michiemo
+ * Date: 19OCT2013
  */
