80d22160d87eb14a5de52cc276fdb8b730569ccb
James Michiemo <james.michiemo@gmail.com>
2013-10-19 02:04:47 -0400

    create 3 instances of the Person object

--- MichiemoJames/Wk3/goal7_assign_objects/js/main.js
+++ MichiemoJames/Wk3/goal7_assign_objects/js/main.js
@@ -8,7 +8,16 @@
 
 (function(){
 
-  var names = ['Jeff','Abed','Britta','Annie','Shirley'];
+  var names = ['Jeff','Abed','Britta','Annie','Shirley'],
+      people = [];
+
+
+  for(var i=0;i<3;i++){
+    var index = ~~(Math.random()*names.length);
+    var person = new Person(names[index],i+1);
+    people.push(person);
+    console.log(people);
+  }
 
 
 
