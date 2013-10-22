32cf900f9dc2b715c5329dc59597f9914dfd0414
James Michiemo <james.michiemo@gmail.com>
2013-10-21 22:41:43 -0400

    setup conditional for each id

--- MichiemoJames/Wk4/goal5_assign_validation/js/validate.js
+++ MichiemoJames/Wk4/goal5_assign_validation/js/validate.js
@@ -10,8 +10,17 @@
 
         //Below is one example of the validateField call with an argument.
         //You must dynamically retrieve the ID name from the DOM/HTML.
-
-        validateField(id);  //id = is the form input field ID
+        var username = document.querySelector('#f_username'),
+            email = document.querySelector('#f_email'),
+            phone = document.querySelector('#f_phone'),
+            ssn = document.querySelector('#f_ssn'),
+            password = document.querySelector('#f_password');
+        
+        validateField(username);  //id = is the form input field ID
+        validateField(email);
+        validateField(phone);
+        validateField(ssn);
+        validateField(password);
 
 
         e.preventDefault();
@@ -21,14 +30,20 @@
 
     var validateField = function(inputName){
 
-        if (inputName.name === 'id name goes here'){
-            var pattern = 'a RegEx pattern goes here'
+        if (inputName.name === 'f_username'){
+            var pattern = /^
 
             //You will need to create an else-if statement for each input field id.  The
             //      format will be similar to the above IF statement.
 
 
+        }else if (inputName.name === 'f_email'){
+        }else if (inputName.name === 'f_phone'){
+        }else if (inputName.name === 'f_ssn'){
+        }else if (inputName.name === 'f_password'){
         };
+          
+
 
         var pass = 'the RegEx .test statement is needed here';
         var errorMsg = inputName.nextSibling.nextSibling.nextSibling.nextSibling;
